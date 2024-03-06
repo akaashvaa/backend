9b18eedac08938cc9dc6eab64705688a52309f96
AkashKumar-0 <ahmvaad@gmail.com>
2024-03-06 16:42:09 +0530

    models and jwt integration

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose , {Schema} from 'mongoose'
-
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 const userSchema = new Schema({
    username : {
     type : String,
@@ -44,6 +45,46 @@ const userSchema = new Schema({
     }
   ]
 }, {timestamps: true})
+
+userSchema.pre("save", async function(next){
+  
+  if(!this.isModified("password")) return  next()
+  
+  this.password = bcrypt.hash(this.password, 10)
+  next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+  jwt.sign(
+    {
+    _id : this._id,
+    email : this.email,
+    username : this.username,
+    fullName : this.fullName
+   },
+    process.env.ACCESSS_TOKEN_SECRET,
+   {
+      expiresIn : process.env.ACCESSS_TOKEN_EXPIRY
+    }
+  )
+}
+  
+userSchema.methods.generateRefreshToken = function (){
+  return jwt.sign(
+    {
+      _id, this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
 
 export const User = mongoose.model("User", userSchema)
 
